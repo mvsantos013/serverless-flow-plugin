177701616c21499269f2291b52f978d6ac706d61
Mateus Santos <mvsantos013@gmail.com>
2024-04-15 20:40:44 -0300

    feat: add schema validation

---
+++ src/schemas.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+import { TaskType } from './types'
+
+export const ServerlessFlowParamsSchema = z.object({
+  resourcesPrefix: z.string().optional().default('ServerlessFlow'),
+  resourcesSuffix: z.string().optional().default('-${self:provider.stage}'),
+  stateMachinesDirectory: z.string().optional().default('./src/stepFunctions'),
+  tasksDirectory: z.string().optional().default('./src/tasks'),
+})
+
+export const EcsTaskParamsSchema = z.object({
+  taskName: z.string(),
+  taskType: z.union([z.literal(TaskType.ECS), z.literal(TaskType.LAMBDA)]),
+  iamRolePolicyStatements: z.array(z.object({})).optional().default([]),
+  cpu: z.number().int(),
+  memory: z.number().int(),
+  ephemeralStorage: z.number().optional().default(20),
+  ecrRepositoryKeepMaxImages: z.number().optional().default(3),
+})
